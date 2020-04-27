12a0121c44d62eeda78dc177b8113b20cb258d0b
mafux777 <mafux777@gmx.de>
2020-04-27 16:26:30 +0100

    about to split off download.py

--- fennel.py
+++ fennel.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
 import argparse
 import pandas as pd
 import psycopg2
-from psycopg2.extras import execute_values
-from collections import namedtuple
 import time
 
 # - provide a time stamped log message
@@ -36,16 +33,21 @@ class DatabaseProxy(object):
         log_me('fetching data...')
         df = pd.read_sql( 
             '''
-            WITH "schema"
+            WITH "datasource" as (SELECT
+                   ds.id as ds_id, 
+                   ds.title as ds_title, 
+                   ds.dbtype as ds_type
+          FROM public.rosemeta_datasource ds where ds.deleted is false),
+"schema"
      AS ( SELECT   s.id AS schema_id,
                    s.name AS schema_name,
                    s.title AS schema_title,
                    s.description AS schema_description,
                    s.ds_id AS ds_id
           FROM     public.rosemeta_schema s
-          WHERE    s.excluded = FALSE
-          ORDER BY ds_id,
-                   schema_id ),
+          INNER JOIN     "datasource" using (ds_id)
+          WHERE    s.excluded = FALSE 
+),
 "table"
      AS ( SELECT   s.ds_id AS ds_id,
                    t."schema" AS schema_name,
@@ -62,6 +64,8 @@ class DatabaseProxy(object):
                    schema_id,
                    t.id )
 SELECT   a.ds_id AS ds_id,
+                   ds_title,
+                   ds_type, 
                    a.schema_id AS schema_id,
                    s.name AS schema_name,
                    a.table_id AS table_id,
@@ -79,6 +83,7 @@ SELECT   a.ds_id AS ds_id,
                    ON a.table_id=t.id
                    JOIN public.rosemeta_schema s
                    ON a.schema_id=s.id
+                   JOIN "datasource" on "datasource".ds_id=a.ds_id
           WHERE    a.excluded = FALSE
                    and table_id IN (SELECT table_id
                                 FROM   "table")
@@ -90,67 +95,15 @@ SELECT   a.ds_id AS ds_id,
             con=self.connection
         )
         return df
-    # a convenience function to correct the data types and eliminate NaN values
-    def write_line(self, row):
-        # convert all IDs and position to int
-        ds_id        = int(row.ds_id)
-        schema_id    = int(row.schema_id)
-        table_id     = int(row.table_id)
-        attribute_id = int(row.attribute_id)
-        position     = int(row.position) if pd.notna(row.position) else None
-
-        # Eliminate NaN values
-        attribute_title = row.attribute_title if pd.notna(row.attribute_title) else None
-        nullable        = row.nullable        if pd.notna(row.nullable)        else None
-        is_primary_key  = row.is_primary_key  if pd.notna(row.is_primary_key)  else None
-        attribute_type  = row.attribute_type  if pd.notna(row.attribute_type)  else 'default'
-
-        database_column = namedtuple('database_column', ['ds_id',
-                    'schema_id',
-                    'schema_name',
-                    'table_id',
-                    'table_name',
-                    'attribute_id',
-                    'attribute_title',
-                    'attribute_description',
-                    'attribute_name',
-                    'attribute_type',
-                    'position',
-                    'nullable',
-                    'is_primary_key'])
-        my_tuple = database_column(ds_id,
-                    schema_id,
-                    row.schema_name,
-                    table_id,
-                    row.table_name,
-                    attribute_id,
-                    attribute_title,
-                    row.attribute_description,
-                    row.attribute_name,
-                    attribute_type,
-                    position,
-                    nullable,
-                    is_primary_key)
-
-        return my_tuple
-    # put all the data into one table on the target
-    def upload_to_remote(self, data, table_name):
-        cur = self.connection.cursor()
-        insert_query = f'insert into {table_name} (ds_id,schema_id,schema_name,table_id,table_name,' \
-            f'attribute_id,attribute_title,attribute_description,attribute_name,attribute_type,position,' \
-            f'nullable,is_primary_key) values %s'
-        execute_values(cur, insert_query, data)
-        self.connection.commit()
-        cur.close()
 
     def send_statement(self, statement, final=False):
         try:
-            log_me(statement)
+            #log_me(statement)
             cursor = self.connection.cursor()
             if not cursor:
                 raise("Connection error")
             cursor.execute(statement)
-            psql_instructions.append(statement)
+            #psql_instructions.append(statement)
             self.connection.commit()
             cursor.close()
         except (Exception, psycopg2.DatabaseError) as error:
@@ -218,105 +171,63 @@ if __name__ == "__main__":
     substitutions={
         r'\(\d+\)': '',
         r'datetime': 'timestamp',
-        r'string': 'text'
+        r'timestamp_ltz': 'timestamp with time zone',
+        r'timestamp_ntz': 'timestamp',
+        r'string': 'text',
+        r'longtext' : 'text',
+        r'number': 'numeric',
+        r'double': 'numeric',
+        r'tinyint': 'integer',
+        r'bytes': 'text',
+        r'geography' : 'polygon',
+        r'na' : 'text'
     }
 
     def modify_attribute(attr):
-        for k, v in substitutions.items():
-            attr = re.sub(k, v, attr)
+        for pattern, replacement in substitutions.items():
+            attr = re.sub(pattern, replacement, attr, flags=re.IGNORECASE)
         return attr
-    data['create_schema']    = 'CREATE SCHEMA IF NOT EXISTS "' + data['schema_name'] + '";\n'
-    data['create_table']     = 'CREATE TABLE IF NOT EXISTS "'  + data['schema_name'] + '"' + '."' + data['table_name'] + '"'
-    data['create_attribute'] = '"' + data['attribute_name'] + '" ' + data['attribute_type'].apply(modify_attribute)
 
-    #schemas = data.groupby(['create_schema']).size().reset_index().iloc[0,:]#.aggregate(lambda x: ",".join(x))
-    schemas = data['create_schema'].unique()
-    tables  = data.groupby(['create_table'])['create_attribute'].aggregate(lambda x: ",".join(x)).reset_index()
+    # data['create_schema']    = 'CREATE SCHEMA IF NOT EXISTS "' + data['schema_name'] + '";\n'
+    # data['create_table']     = 'CREATE TABLE IF NOT EXISTS "'  + data['schema_name'] + '"' + '."' + data['table_name'] + '"'
+    # data['create_attribute'] = '"' + data['attribute_name'] + '" ' + data['attribute_type'].apply(modify_attribute)
 
-    #psql_1 = psql_0.reset_index()
-    tables['create_attribute'] = "(" + tables['create_attribute'] + ");\n"
-    tables=tables.apply(lambda x: " ".join(dict(x).values()), axis=1)
-    # psql_3=tables.sum()
-    # with open("fennel.sql", "w") as psql_4:
-    #     psql_4.write(psql_3)
+    def format_attribute(attr):
+        attr_name = attr['attribute_name']
+        attr_type = modify_attribute(attr['attribute_type'])
+        attr_null = "NOT NULL" if not attr['nullable'] else ""
+        attr_prmk = "PRIMARY KEY" if attr['is_primary_key'] else ""
+        return f'"{attr_name}" {attr_type} {attr_null} {attr_prmk}'
 
+    data['create_attribute'] = data.apply(format_attribute, axis=1)
 
+    # grouped = data.groupby(['schema_name', 'table_name', 'attribute_name'])
+    grouped = data.groupby(['schema_name', 'table_name'])
 
     credentials2 = {
         'HOST': 'fennel2.cluster-cingqyuv6npc.us-east-2.rds.amazonaws.com',
         'PORT': 5433,
-        'USER': 'matthias.funke',
+        'USER': 'postgres',
         'PASSWORD': 'alation123',
-        'NAME': 'fennel'
+        'NAME': 'monday'
     }
-
     exporter2 = DatabaseProxy(credentials2)
-    #data = [exporter2.write_line(t) for t in data.itertuples()]
-
-    ds_created={}
-    schemas_created={}
-    tables_created={}
-    columns_created={}
-
-    psql_instructions=[]
-
-
     # for schema in all_schemas:
-    #     """create schema {schema}"""
-    for statement in list(schemas):
-        exporter2.send_statement(statement)
+    # for schema in set(data['schema_name']):
+    #     statement = f'CREATE SCHEMA IF NOT EXISTS "{schema}"'
+    #     exporter2.send_statement(statement)
+
+    for name, group in grouped:
+        all_attributes = ",".join(set(group['create_attribute']))
+        table = f'CREATE TABLE IF NOT EXISTS "{name[0]}"."{name[1]}"({all_attributes})'
+        exporter2.send_statement(table)
+        table_id = group.iloc[0, 5]
+        table_comment = f'<p>Original source: <a href="https://demo-sales.alationcatalog.com/table/{table_id}/" rel="noopener noreferrer" target="_blank">https://demo-sales.alationcatalog.com/table/{table_id}/</a></p>'
+        exporter2.send_statement(f'COMMENT ON TABLE "{name[0]}"."{name[1]}"' + f" IS '{table_comment}'")
+
     # # create all databases
     # database_name = ...
     # """create database {database_name}"""
-    # # create all schemas
-    # # create all tables
-    for statement in list(tables):
-        exporter2.send_statement(statement)
-    # for table in all_tables:
-    #     """create table {schema}.{table}("""
-    #     for column in all_columns:
-    #     """{column} {attribute_type}"""
-    for item in data: # a list of tuples...
-        # --- Data Source ---
-        if item.ds_id not in ds_created:
-            credentials = {
-                'HOST': 'fennel2.cluster-cingqyuv6npc.us-east-2.rds.amazonaws.com',
-                'PORT': 5433,
-                'USER': 'postgres',
-                'PASSWORD': 'alation123',
-                'NAME': 'formula1'}
-            ds_created[item.ds_id] = dict(
-                statement=f"CREATE DATABASE database_{item.ds_id};",
-                connection=psycopg2.connect(
-            host=credentials['HOST'],
-            port=credentials['PORT'],
-            user=credentials['USER'],
-            password=credentials['PASSWORD'],
-            dbname=credentials['NAME'])#DatabaseProxy(credentials)
-            )
-            # send_statement(ds_created[item.ds_id]['connection'],
-            #                ds_created[item.ds_id]['statement'])
-        # --- Schema ---
-        if item.schema_id not in schemas_created:
-            schemas_created[item.schema_id] = dict(statement=f'CREATE SCHEMA IF NOT EXISTS "{item.schema_name}";')
-            send_statement(ds_created[item.ds_id]['connection'], schemas_created[item.schema_id]['statement'])
-        # --- Table ---
-        if item.table_id not in tables_created:
-            tables_created[item.table_id] = dict(
-                statement=f'CREATE TABLE IF NOT EXISTS "{item.schema_name}"."{item.table_name}" (dummy text);')
-            send_statement(ds_created[item.ds_id]['connection'], tables_created[item.table_id]['statement'])
-        # --- Column ---
-        if item.attribute_id not in columns_created:
-            columns_created[item.attribute_id] = f"{item.attribute_name} {item.attribute_type}"
-            if item.nullable:
-                columns_created[item.attribute_id] += ' NULL'
-            if item.is_primary_key:
-                columns_created[item.attribute_id] += ' PRIMARY KEY'
-
-    with open("my_psql.sql", "w") as psql_file:
-        psql_file.write(psql_instructions.join('\n'))
-
-    exporter2.upload_to_remote(data, "okra")
     exporter2.connection.close()
 
 
