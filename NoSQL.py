3fdbed9874ef58d1a36aac5ec0099df1012f4162
mafux777 <mafux777@gmx.de>
2021-04-27 12:09:48 +0100

    Improved NoSQL to create DS auto

--- NoSQL.py
+++ NoSQL.py
@@ -1,12 +1,12 @@
 import requests
 import json
+import random
 from time import sleep
 from datetime import datetime, timezone
 
 # -- ADJUST THE FOLLOWING CONFIG PARAMS
-ds_id=49
-url = "http://18.218.6.215"
-headers = dict(token='a11b49ca-d484-4a46-b1d9-d0af03ae348c')
+url = "https://2021-2-sandbox.alationproserv.com"
+headers = dict(token='u2WqKUdj2thkUEmbWxhp7HOx_2G9MlAkjXPqnc8R2w4')
 filename = "/Users/matthias.funke/Downloads/ctm_json.json"
 folder='root.bq' # should contain 1 dot
 collection=f'CTM-v4' # should not contain any dots
@@ -16,7 +16,6 @@ def log_me(txt):
     dt = datetime.now(timezone.utc).isoformat(sep=' ', timespec='milliseconds')
     print(f"{dt} {txt}")
 
-api = f"/integration/v1/data/{ds_id}/parse_docstore/"
 
 
 # the dictionary will be the container for all the physical metadata
@@ -67,6 +66,24 @@ def recursive_field_encode(field, parent=None):
     else: # this branch returns a leaf outside
         return {field.pop('name') : field}
 
+
+
+url_1 = url + "/integration/v1/datasource/"
+random = ''.join(random.sample("ABCDEFGHJKLMNPQRSTUVWXYZ0123456789", 4))
+params=dict(dbtype="generic_nosql",
+            title=f"DS {random}",
+            is_virtual=True,
+            deployment_setup_complete=True)
+headers = dict(token='B3vsv3r2MtJlobY6x6f_8jx5d7PiW9V_9WOVrvhadC0')
+r = requests.post(url=url_1, headers=headers, json=params, verify=False)
+
+status = (json.loads(r.content))
+print(status)
+ds_id = status['id']
+print(f"Created data source: {url}/data/{ds_id}/")
+api = f"/integration/v1/data/{ds_id}/parse_docstore/"
+
+
 log_me("Processing recursive fields")
 for f in base:
     name = f.get('name') # will be None if the field does not have a name
@@ -108,85 +125,5 @@ log_me("Uploading logical metadata for NoSQL")
 r2 = requests.post(f'{url}/api/v1/bulk_metadata/custom_fields/default/doc_schema', data=data, headers=headers)
 log_me(r2.content)
 
-# create a virtual data source
-# /integration/v1/datasource/
+print("ALL DONE")
 
-# Parameters for creating the data source
-log_me("Creating a virtual datasource of type bigquery")
-params=dict(dbtype="bigquery", title=f"Ukulele VDS", is_virtual=True, deployment_setup_complete=True)
-r = requests.post(url=f"{url}/integration/v1/datasource/", headers=headers, json=params, verify=False)
-
-# get the status
-status = r.json()
-# extract the data source ID
-ds_id = status['id']
-log_me(f"Created data source: {url}/data/{ds_id}/")
-log_me("Deleting some old data sources")
-for i in range(53, ds_id):
-    r = requests.delete(url=f"{url}/integration/v1/datasource/{i}/", headers=headers, verify=False)
-#ds_id=51
-
-# write physical metadata for VDS
-# at the same time, prepare logical metadata
-body=[dict(key=f'{folder}'), # for the schema
-      dict(key=f'{folder}.{collection}', table_type="TABLE")]
-body_2 = []
-for k, v in physical_metadata.items():
-    segments = k.split(".")
-    seg = len(segments)
-    if seg>4:
-        seg_1 = ".".join(segments[0:3])
-        seg_2 = "_".join(segments[3:])
-        name_2 = f'{seg_1}.{seg_2}'
-        body.append(dict(key=name_2,column_type=v['column_type']))
-        body_2.append(dict(key=name_2,title=f"{seg-3:02} {segments[-1]}", description=logical_metadata[k].get('description')))
-    else:
-        body.append(dict(key=k,column_type=v['column_type']))
-        body_2.append(dict(key=k,title=f"{seg-3:02} {segments[-1]}", description=logical_metadata[k].get('description')))
-
-
-data=""
-for b in body:
-    b['key'] = f"{ds_id}.{b['key']}"
-    data=data+json.dumps(b)+"\n"
-
-log_me("Posting physical metadata for bigquery")
-url_2 = f"{url}/api/v1/bulk_metadata/extraction/{ds_id}"
-r = requests.post(url=url_2, headers=headers, data=data, params=dict(remove_not_seen=True))
-
-# -- Get the status of the bulk upload job ---
-status = r.json()
-params=dict(name=status['job_name'].replace("#", "%23"))
-url_job = f"{url}/api/v1/bulk_metadata/job/?name={params['name']}"
-
-while(True):
-    r_2 = requests.get(url=url_job, headers=headers)
-    status = r_2.json()
-    if status['status'] != 'running':
-        objects = json.loads(status['result'])['error_objects']
-        if objects:
-            for error in objects:
-                log_me(error)
-        else:
-            log_me(status)
-        break
-
-# take care of logical metadata
-data=""
-for b in body_2:
-    b['key'] = f"{ds_id}.{b['key']}"
-    data=data+json.dumps(b)+"\n"
-log_me("Uploading logical metadata")
-r2 = requests.post(f'{url}/api/v1/bulk_metadata/custom_fields/default/mixed', data=data, headers=headers)
-log_me(r2.json())
-
-
-
-# take care of logical metadata
-data=""
-for b in body_2:
-    b['key'] = f"{ds_id}.{b['key']}"
-    data=data+json.dumps(b)+"\n"
-log_me("Uploading logical metadata")
-r2 = requests.post(f'{url}/api/v1/bulk_metadata/custom_fields/default/mixed', data=data, headers=headers)
-log_me(r2.json())
